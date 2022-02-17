dd99fa61ffce4107c1a9f0992ce6bed729c1fa97
VoidGlitch00 <shawonsk09@gmail.com>
2022-02-17 21:23:57 +0600

    second interactive update

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-/* Income & Expenses */
-document.getElementById("final-calculation").addEventListener("click", function () {
-    /* handling all input events */ 
+/* main function */
+function moneyHoney(inputId) {
+    /* handling all input events */
     const incomeInput = document.getElementById("income-input");
     const foodExpenseInput = document.getElementById("food-expense-input");
     const rentExpenseInput = document.getElementById("rent-expense-input");
     const clothesExpenseInput = document.getElementById("clothes-expense-input");
     const showTotalExpense = document.getElementById("total-expense");
     const showFinalBalance = document.getElementById("final-balance");
+    const percentageDigit = document.getElementById("percent-digit");
+    const remainingBalance = document.getElementById("remaining-balance");
     /* taking inputs as value */
     const incomeAmount = parseInt(incomeInput.value);
     const foodAmount = parseInt(foodExpenseInput.value);
@@ -15,9 +17,29 @@ document.getElementById("final-calculation").addEventListener("click", function 
     /* Calculation */
     const totalExpense = foodAmount + rentAmount + clothesAmount;
     const finalBalance = incomeAmount - totalExpense;
-    /* showing balance */
-    showTotalExpense.innerText = totalExpense;
-    showFinalBalance.innerText = finalBalance;
+    /* Percentage Calculation */
+    const percentValue = parseInt(percentageDigit.value);
+    const savingAmount = parseInt(incomeAmount * (percentValue / 100));
+    const savings = document.getElementById("saving-amount");
+    const leftBalance = finalBalance - savingAmount;
+    if (inputId == "incomeAndExpense") {
+        /* showing balance */
+        showTotalExpense.innerText = totalExpense;
+        showFinalBalance.innerText = finalBalance;
+    }
+    else if (inputId == "percentage") {
+        /* showing percentage */
+        savings.innerText = savingAmount;
+        remainingBalance.innerText = leftBalance;
+    }
+}
+
+/* Income & Expenses */
+document.getElementById("final-calculation").addEventListener("click", function () {
+    moneyHoney("incomeAndExpense");
 })
 
 /* Percentage Calculation */
+document.getElementById("percentage-calculation").addEventListener("click", function () {
+    moneyHoney("percentage");
+})
